73650885687d534020d433096437ceb23ec8e1e2
hwayoung kim <hwayoung.kim@lemonhc.com>
2022-08-03 09:25:17 +0900

    일부 코드 수정

--- src/test/StringBaseHash.ts
+++ src/test/StringBaseHash.ts
@@ -2,14 +2,17 @@ import * as crypto from "crypto";
 import {TextEncoder} from "util";
 
 let nonce = 0
-const sysdate: Date = new Date()
+const sysdate: number = Date.now()
 
 async function generateHash(input: string): Promise<string> {
-    const msgBuffer = new TextEncoder().encode(input);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('')
-    return hashHex
+    // const msgBuffer = new TextEncoder().encode(input);
+    // const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+    // const hashArray = Array.from(new Uint8Array(hashBuffer))
+    // const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('')
+    // return hashHex
+
+    // window 에서는 위의 코드가 동작되지 않아, 아래의 코드로 변경
+    return crypto.createHash('sha256').update(input).digest('hex')
 }
 
 async function calculateHashWithNonce(nonce: number): Promise<string>{
@@ -24,6 +27,7 @@ async function mine(difficulty: number): Promise<void> {
         hash = await calculateHashWithNonce(++nonce)
     }while(!hash.startsWith('0'.repeat(difficulty)))
     console.log(`Hash: ${hash}, nonce: ${nonce}`)
-    console.log(`during: ${Date.now() - sysdate.getTime()}ms`)
+    console.log(`during: ${Date.now() - sysdate}ms`)
 }
-mine(5)
+
+mine(6)
